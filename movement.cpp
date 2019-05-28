9a6440ef127a78be7d9301f617c38894af2898c0
Bruno Glecer <bruno.glecer@gmail.com>
2019-05-28 11:24:05 -0300

    Added TODO comments, changed variable names

--- movement.cpp
+++ movement.cpp
@@ -1,155 +1,151 @@
+#include "gyro.h"
 #include "movement.h"
 #include "motors.h"
-#include "gyro.h"
 
 #define FORWARD 1
 #define ROTATING 2
 
-int prevError;
-int prevAngleError;
-int prevAngularSpeedError;
-int prevMotorDifferenceError;
+int prevForwardSpeedError;
+int prevForwardDifferenceError;
+int prevRotatingSpeedError;
+int prevRotatingDifferenceError;
 
-unsigned long integral = 0;
-unsigned long angleIntegral = 0;
-unsigned long motorDifferenceIntegral = 0;
-unsigned long angularSpeedIntegral = 0;
-
-
+unsigned long forwardSpeedIntegral = 0;
+unsigned long forwardDifferenceIntegral = 0;
+unsigned long rotatingDifferenceIntegral = 0;
+unsigned long rotatingSpeedIntegral = 0;
 
 char movementState = FORWARD;
 
-
-
-int longTermSpeedTarget = 0;
-int shortTermSpeedTarget = 0;
-int shortTermAngularSpeedTarget = 0;
+int longTermForwardSpeedTarget = 0;
+int shortTermForwardSpeedTarget = 0;
+int shortTermRotatingSpeedTarget = 0;
 
 int angleOffset = 0;
 
-void resetAngle(){
-  angleOffset = gyro_getAngle();
-  angleIntegral = 0;
-  integral = 0;
+void resetAngle() {  //TODO: Move to gyro.cpp
+    angleOffset = gyro_getAngle();
 }
 
+int angleTarget;  //TODO: Implement functionality.
 
-int angleTarget;
+void updatePID() {         //THIS FUNCTION NEEDS TO BE CALLED PERIODICALLY. TODO: Maybe use a timer to call it.
+    motors_recordSpeed();  //Records motor speed, to leftMotorSpeed and rightMotorSpeed
 
-int RticksCount = 0;
-int LticksCount = 0;
-
-
-void updatePID() {
-  motors_recordSpeed();
-
-  if(movementState == FORWARD){
-  if(longTermSpeedTarget > 0){
-    if (shortTermSpeedTarget < longTermSpeedTarget) { //Update shortTermSpeedTarget based on current longTermSpeedTarget and ACCEL
-      shortTermSpeedTarget += ((long)ACCEL * ENCODER_SPEED_SAMPLE_INTERVAL) / 1000;
-      if (shortTermSpeedTarget > longTermSpeedTarget) {
-        shortTermSpeedTarget = longTermSpeedTarget;
-      }
-    }
-    else if (shortTermSpeedTarget > longTermSpeedTarget) {
-      shortTermSpeedTarget -= ((long)ACCEL * ENCODER_SPEED_SAMPLE_INTERVAL) / 1000;
-      if (shortTermSpeedTarget < longTermSpeedTarget) {
-        shortTermSpeedTarget = longTermSpeedTarget;
-      }
-    }
-  }
-  else {
-    shortTermSpeedTarget = 0;
-    motors_break();
-  }
-  if(shortTermSpeedTarget > 0){
-      int error = shortTermSpeedTarget - (leftMotorSpeed + rightMotorSpeed) / 2;
-
-      if (abs(integral + error) < 255 / Ki)
-        integral += error;
-      int power = Kp * error + Kd * (error - prevError) + Ki * integral;
-      prevError = error;
-
-      int angleError = (-gyro_getAngle() + angleOffset);
-
-
-      if (abs(angleIntegral + angleError) < 255 / Kig)
-        angleIntegral += angleError;
-      int anglePower = Kpg * angleError + Kdg * (angleError - prevAngleError) + Kig * angleIntegral;
-      prevAngleError = angleError;
-      Serial.print(power);
-      Serial.print(" ");
-      Serial.println(error);
-
-      int lpower = power - anglePower;
-      int rpower = power + anglePower;
-
-      if (lpower > MAX_MOTOR_POWER)lpower = MAX_MOTOR_POWER;
-      else if (lpower < -MAX_MOTOR_POWER)lpower = -MAX_MOTOR_POWER;
-
-      if (rpower > MAX_MOTOR_POWER)rpower = MAX_MOTOR_POWER;
-      else if (rpower < -MAX_MOTOR_POWER)rpower = -MAX_MOTOR_POWER;
-
-      motors_setPower(lpower, rpower);
-
-    }
-  }
-  else if(movementState == TURNING){
-
-
-
-      if (shortTermAngularSpeedTarget < TURNING_SPEED) {
-        shortTermAngularSpeedTarget += ((long)TURNING_ACCEL * ENCODER_SPEED_SAMPLE_INTERVAL) / 1000;
-        if (shortTermAngularSpeedTarget > TURNING_SPEED) {
-          shortTermAngularSpeedTarget = TURNING_SPEED;
+    if (movementState == FORWARD) {
+        if (longTermForwardSpeedTarget > 0) {                                //If desired speed > 0, adjust short term value based on acceleration
+            if (shortTermForwardSpeedTarget < longTermForwardSpeedTarget) {  //Update shortTermForwardSpeedTarget based on current longTermForwardSpeedTarget and ACCEL
+                shortTermForwardSpeedTarget += ((long)ACCEL * ENCODER_SPEED_SAMPLE_INTERVAL) / 1000;
+                if (shortTermForwardSpeedTarget > longTermForwardSpeedTarget) {
+                    shortTermForwardSpeedTarget = longTermForwardSpeedTarget;
+                }
+            } else if (shortTermForwardSpeedTarget > longTermForwardSpeedTarget) {
+                shortTermForwardSpeedTarget -= ((long)ACCEL * ENCODER_SPEED_SAMPLE_INTERVAL) / 1000;
+                if (shortTermForwardSpeedTarget < longTermForwardSpeedTarget) {
+                    shortTermForwardSpeedTarget = longTermForwardSpeedTarget;
+                }
+            }
+        } else {  //If desired speed is 0, break motors.
+            shortTermForwardSpeedTarget = 0;
+            motors_break();
         }
-      }
-      else if (shortTermAngularSpeedTarget > TURNING_SPEED) {
-        shortTermAngularSpeedTarget -= ((long)TURNING_ACCEL * ENCODER_SPEED_SAMPLE_INTERVAL) / 1000;
-        if (shortTermAngularSpeedTarget < TURNING_SPEED) {
-          shortTermAngularSpeedTarget = TURNING_SPEED;
+
+        if (shortTermForwardSpeedTarget > 0) { //If motors didn't break, or current short term desired speed is > 0
+
+            //Speed control PID loop
+            int error = shortTermForwardSpeedTarget - (leftMotorSpeed + rightMotorSpeed) / 2;
+            if (abs(forwardSpeedIntegral + error) < 255 / Ki)
+                forwardSpeedIntegral += error;
+            int power = Kp * error + Kd * (error - prevForwardSpeedError) + Ki * forwardSpeedIntegral;
+            prevForwardSpeedError = error;
+
+
+            //Ange correction PID loop
+            int angleError = (-gyro_getAngle() + angleOffset);
+            if (abs(forwardDifferenceIntegral + angleError) < 255 / Kig)
+                forwardDifferenceIntegral += angleError;
+            int anglePower = Kpg * angleError + Kdg * (angleError - prevForwardDifferenceError) + Kig * forwardDifferenceIntegral;
+            prevForwardDifferenceError = angleError;
+
+            //Calculate motor powers.
+            int lpower = power - anglePower;
+            int rpower = power + anglePower;
+
+            if (lpower > MAX_MOTOR_POWER) //TODO: Move power limit to motors.cpp
+                lpower = MAX_MOTOR_POWER;
+            else if (lpower < -MAX_MOTOR_POWER)
+                lpower = -MAX_MOTOR_POWER;
+
+            if (rpower > MAX_MOTOR_POWER)
+                rpower = MAX_MOTOR_POWER;
+            else if (rpower < -MAX_MOTOR_POWER)
+                rpower = -MAX_MOTOR_POWER;
+
+            motors_setPower(lpower, rpower);//Update motor powers.
         }
-      }
-    
-      int angularSpeedError = shortTermAngularSpeedTarget - (leftMotorSpeed + rightMotorSpeed) / 2;
 
-      if (abs(angularSpeedIntegral + angularSpeedError) < 255 / Ki_rot)
-        angularSpeedIntegral += angularSpeedError;
-      int totalPower = Kp_rot * angularSpeedError + Kd_rot * (angularSpeedError - prevAngularSpeedError) + Ki_rot * angularSpeedIntegral;
-      prevAngularSpeedError = angularSpeedError;
+    } else if (movementState == TURNING) {
 
-      int motorDifferenceError = leftMotorSpeed - rightMotorSpeed;
+        if (shortTermRotatingSpeedTarget < TURNING_SPEED) { //Update short trem rotation speed based on TURNING_ACCEL
+            shortTermRotatingSpeedTarget += ((long)TURNING_ACCEL * ENCODER_SPEED_SAMPLE_INTERVAL) / 1000;
+            if (shortTermRotatingSpeedTarget > TURNING_SPEED) {
+                shortTermRotatingSpeedTarget = TURNING_SPEED;
+            }
+        } else if (shortTermRotatingSpeedTarget > TURNING_SPEED) {
+            shortTermRotatingSpeedTarget -= ((long)TURNING_ACCEL * ENCODER_SPEED_SAMPLE_INTERVAL) / 1000;
+            if (shortTermRotatingSpeedTarget < TURNING_SPEED) {
+                shortTermRotatingSpeedTarget = TURNING_SPEED;
+            }
+        }
 
-      if (abs(motorDifferenceIntegral + motorDifferenceError) < 255 / Kig)
-        motorDifferenceIntegral += motorDifferenceError;
-      int differencePower = Kp_diff * motorDifferenceError + Kd_diff * (motorDifferenceError - prevMotorDifferenceError) + Ki_diff * motorDifferenceIntegral;
-      prevMotorDifferenceError = motorDifferenceError;
- 
+        //Total motor velocity PID loop
+        int angularSpeedError = shortTermRotatingSpeedTarget - (leftMotorSpeed + rightMotorSpeed) / 2;
+        if (abs(rotatingSpeedIntegral + angularSpeedError) < 255 / Ki_rot)
+            rotatingSpeedIntegral += angularSpeedError;
+        int totalPower = Kp_rot * angularSpeedError + Kd_rot * (angularSpeedError - prevRotatingSpeedError) + Ki_rot * rotatingSpeedIntegral;
+        prevRotatingSpeedError = angularSpeedError;
 
-      int lpower = -totalPower + differencePower;
-      int rpower = totalPower - differencePower;
 
-      if (lpower > MAX_MOTOR_POWER)lpower = MAX_MOTOR_POWER;
-      else if (lpower < -MAX_MOTOR_POWER)lpower = -MAX_MOTOR_POWER;
+        //Same motor speed correction PID loop
+        int motorDifferenceError = leftMotorSpeed - rightMotorSpeed;
+        if (abs(rotatingDifferenceIntegral + motorDifferenceError) < 255 / Kig)
+            rotatingDifferenceIntegral += motorDifferenceError;
+        int differencePower = Kp_diff * motorDifferenceError + Kd_diff * (motorDifferenceError - prevRotatingDifferenceError) + Ki_diff * rotatingDifferenceIntegral;
+        prevRotatingDifferenceError = motorDifferenceError;
 
-      if (rpower > MAX_MOTOR_POWER)rpower = MAX_MOTOR_POWER;
-      else if (rpower < -MAX_MOTOR_POWER)rpower = -MAX_MOTOR_POWER;
+        //Calculate motor powers
+        int lpower = -totalPower + differencePower;
+        int rpower =  totalPower - differencePower;
 
-      motors_setPower(lpower, rpower);
-  }
+        if (lpower > MAX_MOTOR_POWER) //TODO: Move to motors.cpp
+            lpower = MAX_MOTOR_POWER;
+        else if (lpower < -MAX_MOTOR_POWER)
+            lpower = -MAX_MOTOR_POWER;
+
+        if (rpower > MAX_MOTOR_POWER)
+            rpower = MAX_MOTOR_POWER;
+        else if (rpower < -MAX_MOTOR_POWER)
+            rpower = -MAX_MOTOR_POWER;
+
+        motors_setPower(lpower, rpower);//Set motor powers.
+    }
 }
 
+//Set movement state to FORWARD. Tries to mantain a constant forward speed.
+//Accelerates linearly at ACCEL rate until robotSpeed speed is reached.
 void forward(int robotSpeed) {
-  longTermSpeedTarget = robotSpeed;
-  movementState = FORWARD;
+    longTermForwardSpeedTarget = robotSpeed;
+    movementState = FORWARD;
 }
 
-void turnFor(int angle){
-  movementState = TURNING;
-  resetAngle();
-  motorDifferenceIntegral = 0;
-  angularSpeedIntegral = 0;
-  angleTarget = angle*10;
-  totalLeftEncoderCount = 0;
-  totalRightEncoderCount = 0;
+//Rotates on axis. TODO: Implement correct acceletation and deacceleration. And stop when angle is reached.
+void turnFor(int angle) {
+    movementState = TURNING;
+    resetAngle();
+    rotatingDifferenceIntegral = 0;
+    rotatingSpeedIntegral = 0;
+    angleTarget = angle * 10;
+    totalLeftEncoderCount = 0;
+    totalRightEncoderCount = 0;
 }
+
--- movement.h
+++ movement.h
@@ -4,44 +4,44 @@
 #include <Arduino.h>
 #include "sensors.h"
 
-#define MAX_MOTOR_POWER 200
-#define MAX_MOTOR_TURNING_POWER 150
+#define MAX_MOTOR_POWER 200             //Limit motor power for safety reasons.
 
-#define Ki  0.04
+#define Ki  0.04  //PID Values for keeping robot at a constant linear velocity. Encoder based.
 #define Kp  0.07
 #define Kd  0
 
-#define Kig  0
+#define Kig  0  //PID Values for keeping robot straight.  Gyroscope based.
 #define Kpg  0.1
 #define Kdg  0.08
 
 
-#define Ki_rot   0.04
+#define Ki_rot   0.04  //PID Values for keeping robot turning at a constant angular velocity. Encoder based.
 #define Kp_rot  0.07
 #define Kd_rot  0
 
-#define Ki_diff   0
+#define Ki_diff   0  //PID Values for keeping robot turing on its central axis. Encoder based.
 #define Kp_diff  0.1
 #define Kd_diff  0.08
 
+enum states{FORWARD, TURNING};
 
-#define STRAIGHT 1
-#define TURNING 2
+#define TURNING_SPEED 6000 //Maximum angular velocity reached. TODO: Make the velocity setable by rotate function.
+#define TURNING_ACCEL 300  //TODO: Implementation of accel. won't work for deacceleration, it needs to be based on angle position.
 
-#define TURNING_SPEED 6000
-#define TURNING_ACCEL 300 
+#define ACCEL 4000  //Acceleration and deacceleration for linear movement.
 
-#define ACCEL 4000
+#define INTEGRAL_LIMIT 500 //TODO: Implement correct integral windup correction.
+
+extern unsigned long forwardSpeedIntegral;
+extern unsigned long forwardDifferenceIntegral;
+extern unsigned long rotatingDifferenceIntegral;
+extern unsigned long rotatingSpeedIntegral;
 
 
-#define INTEGRAL_LIMIT 500
-
-extern unsigned long integral;
-extern unsigned long angleIntegral;
-extern int prevError;
-extern int prevAngleError;
-extern long rotateIntegral;
-extern int angleTarget;
+extern int prevForwardSpeedError;
+extern int prevForwardDifferenceError;
+extern int prevRotatingSpeedError;
+extern int prevRotatingDifferenceError;
 
 extern char robotState;
 
